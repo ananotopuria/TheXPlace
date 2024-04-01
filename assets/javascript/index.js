ce22cdd41566c909080816cdf554849fa14ad162
Anano Topuria <anano.topuriaa@gmail.com>
2024-04-01 22:46:17 +0400

    feat: add slider and card sections

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,3 +1,12 @@
 import { initStickyObserver } from "./stickyObserver.js";
 
 initStickyObserver(".point");
+
+
+const slider = document.querySelector('.companies__slider');
+
+slider.addEventListener('animationiteration', () => {
+  const firstImage = slider.querySelector('img:first-child');
+  slider.appendChild(firstImage.cloneNode(true));
+  slider.removeChild(firstImage);
+});
